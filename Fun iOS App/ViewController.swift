bf1f58002e8b79451d8a7f87162290db80c61238
grammydavisjr <grammydavisjr@gmail.com>
2017-11-05 12:48:15 -0500

    Initial Commit

---
+++ Fun iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun iOS App
+//
+//  Created by Brian Gibbs  on 11/5/17.
+//  Copyright © 2017 My Org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
