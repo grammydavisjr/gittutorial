fb492f715042d16cb7faf9b23dbdc47f2af9e2b4
grammydavisjr <grammydavisjr@gmail.com>
2017-11-05 13:53:33 -0500

    just a new commit to test

--- Fun iOS App/ViewController.swift
+++ Fun iOS App/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func clickingTheButton(_ sender: Any) {
+        
+        
+        theLabel.text = "Clicked button!"
+    
+    }
 
 }
 
